ba19be1b3024edce204fbb62750f516e9ddc200f
Swapkashwed <102313176+Swapkashwed@users.noreply.github.com>
2022-04-12 19:07:12 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "swap-sk-demo"
-    key            = "skworkspace"
+    key            = "skworkspacenew"
     region         = "ap-south-1"
     dynamodb_table = "swap_terra"
   }
